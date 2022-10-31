6de6166f1070f87b26ab18f20d1f81eec8e56041
Michael Chop <007micle007@gmail.com>
2022-10-31 19:22:45 +0300

    Added class Site to the project

--- src/app.js
+++ src/app.js
@@ -1,20 +1,7 @@
 import {model} from './model'
+import {Site} from './classes/site'
 import './styles/main.css'
 
-const $site = document.querySelector('#site')
+const site = new Site('#site')
 
-model.forEach(block => {
-    // let html = ''
-    // if (block.type === 'title') {
-    //     html = title(block)
-    // } else if (block.type === 'text') {
-    //     html = text(block)
-    // } else if (block.type === 'columns') {
-    //     html = columns(block)
-    // } else if (block.type === 'image') {
-    //     html = image(block)
-    // }
-    // const toHTML = templates[block.type]
-
-    $site.insertAdjacentHTML('beforeend', block.toHTML())
-})
+site.render(model)
