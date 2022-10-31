eae9da21541cff3f160ecd0c4a82da96a34d48a7
Michael Chop <007micle007@gmail.com>
2022-10-31 19:01:15 +0300

    Rewrote the functionality of adding elements using classes

--- src/model.js
+++ src/model.js
@@ -1,61 +1,100 @@
 import image from './assets/image.jpg'
+import {Block} from './classes/blocks'
 
 export const model = [
-    {
-        type: 'title',
-        value: 'Конструктор сайтов на чистом JavaScript',
-        options: {
-            tag: 'h2',
-            // styles: `background: linear-gradient(to right, #ff0099, #493240);color: #fff;text-align: center;padding: 1.5rem`
-            styles: {
-                background: 'linear-gradient(to right, #ff0099, #493240)',
-                color: '#fff',
-                'text-align': 'center',
-                padding: '1.5rem'
-            }
+    // {
+    //     type: 'title',
+    //     value: 'Конструктор сайтов на чистом JavaScript',
+    //     options: {
+    //         tag: 'h2',
+    //         // styles: `background: linear-gradient(to right, #ff0099, #493240);color: #fff;text-align: center;padding: 1.5rem`
+    //         styles: {
+    //             background: 'linear-gradient(to right, #ff0099, #493240)',
+    //             color: '#fff',
+    //             'text-align': 'center',
+    //             padding: '1.5rem'
+    //         }
+    //     }
+    // },
+    new Block('title', 'Конструктор сайтов на чистом JavaScript', {
+        tag: 'h2',
+        styles: {
+            background: 'linear-gradient(to right, #ff0099, #493240)',
+            color: '#fff',
+            'text-align': 'center',
+            padding: '1.5rem'
         }
-    },
-    {
-        type: 'text',
-        value: 'Этот текст очень важен, поэтому он здесь',
-        options: {
+    }),
+    // {
+    //     type: 'text',
+    //     value: 'Этот текст очень важен, поэтому он здесь',
+    //     options: {
+    //         styles: {
+    //             background: 'linear-gradient(to left, #f2994a, #f2c94c)',
+    //             padding: '1rem',
+    //             'font-weight': 'bold'
+    //         }
+    //     }
+    // },
+    new Block('text', 'Этот текст очень важен, поэтому он здесь', {
             styles: {
                 background: 'linear-gradient(to left, #f2994a, #f2c94c)',
                 padding: '1rem',
                 'font-weight': 'bold'
             }
-        }
-    },
-    {
-        type: 'image',
-        value: image,
-        options: {
-            styles: {
-                padding: '2rem 0',
-                display: 'flex',
-                'justify-content': 'center',
-            },
-            imageStyles: {
-                width: '500px',
-                height: 'auto'
-            },
-            alt: 'Website picture'
-        }
-    },
-    {
-        type: 'columns',
-        value: [
-            'Приложение на чистом JavaScript',
-            'Свой конструктор сайтов',
-            'Это очень крутой конструктор',
-        ],
-        options: {
-            styles: {
-                background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
-                padding: '2rem',
-                color: '#fff',
-                'font-weight': 'bold'
-            }
-        }
-    }
+        }),
+    // {
+    //     type: 'image',
+    //     value: image,
+    //     options: {
+    //         styles: {
+    //             padding: '2rem 0',
+    //             display: 'flex',
+    //             'justify-content': 'center',
+    //         },
+    //         imageStyles: {
+    //             width: '500px',
+    //             height: 'auto'
+    //         },
+    //         alt: 'Website picture'
+    //     }
+    // },
+    new Block('image', image, {
+        styles: {
+            padding: '2rem 0',
+            display: 'flex',
+            'justify-content': 'center',
+        },
+        imageStyles: {
+            width: '500px',
+            height: 'auto'
+        },
+        alt: 'Website picture'
+    }),
+    // {
+    //     type: 'columns',
+    //     value: [
+    //         'Приложение на чистом JavaScript',
+    //         'Свой конструктор сайтов',
+    //         'Это очень крутой конструктор',
+    //     ],
+    //     options: {
+    //         styles: {
+    //             background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
+    //             padding: '2rem',
+    //             color: '#fff',
+    //             'font-weight': 'bold'
+    //         }
+    //     }
+    // },
+    new Block('columns', [
+        'Приложение на чистом JavaScript',
+        'Свой конструктор сайтов',
+        'Это очень крутой конструктор',
+    ], {styles: {
+            background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
+            padding: '2rem',
+            color: '#fff',
+            'font-weight': 'bold'
+        }}),
 ]
